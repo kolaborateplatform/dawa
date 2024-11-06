e4b6edd6fef72def4fa4f89bbdfdaf97af42d547
Ochieng Paul <ochiengpaul442@gmail.com>
2024-11-06 21:37:01 +0300

    Dawa website initial project setup

---
+++ dawa-website/src/__tests__/components/Sample.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Sample from '@/components/common/Sample';
+
+describe('HelloWorld', () => {
+  it('renders "Hello, World" text', () => {
+    render(<Sample />);
+    const heading = screen.getByRole('heading', { name: /hello, world/i });
+    expect(heading).toBeInTheDocument();
+  });
+});
