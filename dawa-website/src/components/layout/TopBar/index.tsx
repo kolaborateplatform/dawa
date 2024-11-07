7daa467ea8c286a5e40c899baa3f749e8e0763b1
Ochieng Paul <ochiengpaul442@gmail.com>
2024-11-08 02:23:52 +0300

    home page setup

--- dawa-website/src/components/layout/TopBar/index.tsx
+++ dawa-website/src/components/layout/TopBar/index.tsx
@@ -4,16 +4,18 @@ const TopBar = () => {
   return (
     <div className="bg-primary_1 text-white py-2">
       <div className="container mx-auto flex justify-between items-center px-4">
-        {/* Left Section */}
-        <div className="flex items-center space-x-2">
-          <FaPhoneAlt />
-          <span>+256 702108552</span>
-        </div>
+        <div className="flex justify-start gap-4 items-center">
+          {/* Left Section */}
+          <div className="flex items-center space-x-2">
+            <FaPhoneAlt />
+            <span>+256 702108552</span>
+          </div>
 
-        {/* Center Section */}
-        <div className="flex items-center space-x-2">
-          <FaEnvelope />
-          <span>dawaonlinestore@gmail.com</span>
+          {/* Center Section */}
+          <div className="flex items-center space-x-2">
+            <FaEnvelope />
+            <span>dawaonlinestore@gmail.com</span>
+          </div>
         </div>
 
         {/* Right Section */}
