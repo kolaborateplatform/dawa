e2d9c8d9d20904204863ea61d947b80bff557a64
Ochieng Paul <ochiengpaul442@gmail.com>
2024-11-12 01:12:47 +0300

    removed files

--- dawa-website/src/app/(main)/categories/layout.tsx
+++
@@ -1,8 +0,0 @@
-import Layout from '@/components/layout';
-export default function CategoryLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <Layout newsletterProps={{ container: false }}>{children}</Layout>;
-}
--- dawa-website/src/app/(main)/categories/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import CategoriesPage from './CategoriesPage';
-
-const page = () => {
-  return <CategoriesPage />;
-};
-
-export default page;
